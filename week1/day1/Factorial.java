d5a8ba33eeb49e2bff893898254a3064edf4bab5
G-DeepakSrinivas <128232214+G-DeepakSrinivas@users.noreply.github.com>
2023-03-29 21:13:06 +0530

    Add files via upload

---
+++ week1/day1/Factorial.java
@@ -0,0 +1,13 @@
+
+public class Factorial {
+
+	public static void main(String[] args) {
+		int num = 5;
+		int fac = 1;
+		for (int i=1; i<=num;i++) {
+		fac=fac*i;
+		}
+System.out.println(fac);
+	}
+
+}
---
+++ week1/day1/OddorEven.java
@@ -0,0 +1,16 @@
+
+public class OddorEven {
+
+	public static void main(String[] args) {
+		int num = 15;
+		if (num%2!=0) {
+		System.out.println("The given number is odd");
+		
+		}
+		else {
+			System.out.println("The given number is not odd");
+		}
+
+	}
+
+}
