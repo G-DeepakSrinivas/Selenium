75f96bcf8764abbd1b96dfc95416cbc018de29e8
G-DeepakSrinivas <128232214+G-DeepakSrinivas@users.noreply.github.com>
2023-03-29 21:08:37 +0530

    Add files via upload
    day2

---
+++ week/day2/DeepakSri.java
@@ -0,0 +1,11 @@
+package week.day2;
+
+public class DeepakSri {
+	//type main -->press ctrl+ enter
+	public static void main(String[] args) {
+		//type syso --> press ctrl+space
+		System.out.println("Hello Testleaf");
+		
+	}
+
+}
